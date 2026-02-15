7ed1e71c1f1863d077a8385ee1e067ff0679c847
lava-m <lavanya.martin1@gmail.com>
2026-02-15 21:06:32 +1100

    Playing with the routes

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ console.log("Starting a new project");
 // Express is a library.
 const express = require('express');
 
-
-
 // factory to buid web servers
 // start the server
 const app = express();
@@ -15,9 +13,32 @@ const app = express();
 //request handler function
 // listening to requests
 app.use("/test", (req, res)=>{
-    res.send("Response, I love listening to you")
+    res.send("Response, I love listening to you 1")
 });
+
+// use will match all the HTTP method API calls to /test
+app.get("/user", (req, res)=>{
+    console.log(req.query);
+    res.send({ firstName: "Akshay", lastName: "Saini"});
+});
+
+app.post("/user", (req, res)=> {
+    res.send("Hello from the server");
+}) 
 
 app.listen(3000, ()=>{
     console.log("Successfully listening on port 3000");
+});
+
+app.delete("/user", (req, res)=>{
+    res.send("Delete request");
+});
+
+app.put("/user", (req, res)=>{
+    res.send("put request");
+});
+
+app.patch("/user", (req, res)=>{
+    console.log(req.query);
+    res.send("Patch request");
 });
