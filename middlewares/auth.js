3d90007dbc9740a4df712b8c4d238125a869e33c
lava-m <lavanya.martin1@gmail.com>
2026-03-05 22:28:35 +1100

    Adding jwt in middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,34 @@
 
 // middlewares/auth.js
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
-export const adminAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-  
-    if (!isAdminAuthorized) {
-      return res.status(401).send("Unauthorized request");
+  export const userAuth = async(req, res, next) => {
+
+    try{
+      const {token} = req.cookies;
+
+      if(!token){
+        throw new Error("Token is not valid!!");
+      }
+
+      const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+      const {_id} = decodedObj;
+
+      const user = await User.findById(decodedObj._id);
+
+      if(!user){
+        throw new Error("User not found");
+        }
+        req.user = user;
+        next();// move to the next handler
     }
-  
-    next();
-  };
-  
-  export const userAuth = (req, res, next) => {
-    console.log("User auth is getting checked");
-    const token = "xyz";
-    const isUserAuthorized = token === "xyz";
-  
-    if (!isUserAuthorized) {
-      return res.status(401).send("Unauthorized request");
+    catch(err){
+          res.status(400).send("ERROR:"+ err.message);
     }
-  
-    next();
-  };
+};
+
 
 
 
